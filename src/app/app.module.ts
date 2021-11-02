2a3be2690a9e3fdf683b47dadb293e0f32ced9ce
Kyle Saunders <Kyle.Saunders1310@gmailo.com>
2021-11-02 14:04:11 +0200

    Created navbar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
