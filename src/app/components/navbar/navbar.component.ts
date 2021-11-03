7c922b5a6eae764f0a40dd11d734e5a050909e3b
Kyle Saunders <Kyle.Saunders1310@gmailo.com>
2021-11-03 13:09:59 +0200

    Made navbar items responsive to navigation.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  currentRoute = '';
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.router.events.subscribe(result => {
+      if (result instanceof NavigationEnd) {
+        this.currentRoute = result.urlAfterRedirects
+      }
+    }, error => {
+      //TODO: Add error catching. 
+    })
   }
-
 }
