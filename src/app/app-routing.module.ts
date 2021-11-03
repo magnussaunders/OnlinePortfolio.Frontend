7c4447e02f09fe5f0891209d91a75a2edaa07ede
Kyle Saunders <Kyle.Saunders1310@gmailo.com>
2021-11-03 10:33:02 +0200

    Created routes for pages.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMePageComponent } from './components/about-me-page/about-me-page.component';
+import { CareerPageComponent } from './components/career-page/career-page.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { ProjectsPageComponent } from './components/projects-page/projects-page.component';
 
 const routes: Routes = [
-  { path: '', component: LandingPageComponent}
+  { path: '', component: LandingPageComponent},
+  { path: '/About Me', component: AboutMePageComponent},
+  { path: '/Career', component: CareerPageComponent},
+  { path: '/Projects', component: ProjectsPageComponent}
 ];
 
 @NgModule({
