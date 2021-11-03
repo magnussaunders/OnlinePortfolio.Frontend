30ce473969cfde1d1a85784848bcf58ba04218eb
Kyle Saunders <Kyle.Saunders1310@gmailo.com>
2021-11-03 10:12:30 +0200

    Created components for pages.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AboutMePageComponent } from './components/about-me-page/about-me-page.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { CareerPageComponent } from './components/career-page/career-page.component';
+import { ProjectsPageComponent } from './components/projects-page/projects-page.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
+    AboutMePageComponent,
+    LandingPageComponent,
+    CareerPageComponent,
+    ProjectsPageComponent,
   ],
   imports: [
     BrowserModule,
