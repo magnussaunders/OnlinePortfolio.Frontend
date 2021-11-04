25dce35e39ef660a47307184602dea77ff1ab968
Kyle Saunders <Kyle.Saunders1310@gmailo.com>
2021-11-04 08:24:32 +0200

    Created journey carousol.

--- src/app/components/about-me-page/about-me-page.component.ts
+++ src/app/components/about-me-page/about-me-page.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 @Component({
   selector: 'app-about-me-page',
   templateUrl: './about-me-page.component.html',
   styleUrls: ['./about-me-page.component.scss']
 })
 export class AboutMePageComponent implements OnInit {
+  @ViewChild('journeyItems') journeyItemsRef: any
+  public journeyCurrentIndex = 1;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  journeyNavigatePrevious() {
+    if (this.journeyCurrentIndex === 1) {
+      this.journeyCurrentIndex = 4
+      document.getElementById('journey-items')!.style.transform = "translate(-66.66%, 0%)"
+    } else {
+      document.getElementById('journey-items')!.style.transform = "translate(" + ((this.journeyCurrentIndex - 3) * -33.33).toString() + "%, 0%)"
+      this.journeyCurrentIndex = this.journeyCurrentIndex - 1
+    }
+  }
+
+  journeyNavigateNext() {
+    if (this.journeyCurrentIndex === 4) {
+      this.journeyCurrentIndex = 1
+      document.getElementById('journey-items')!.style.transform = "translate(33.33%, 0%)"
+    } else {
+      document.getElementById('journey-items')!.style.transform = "translate(" + ((this.journeyCurrentIndex - 1) * -33.33).toString() + "%, 0%)"
+      this.journeyCurrentIndex = this.journeyCurrentIndex + 1
+    }
+  }
+
+  journeyNavigateTo(index: number) {
+      if (index) {
+        this.journeyCurrentIndex = index;
+        document.getElementById('journey-items')!.style.transform = "translate(" + ((this.journeyCurrentIndex - 2) * -33.33).toString() + "%, 0%)"
+      }
   }
 
 }
