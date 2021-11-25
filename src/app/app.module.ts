d02430877e322f97198c969063454d53455779dd
Kyle Saunders <Kyle.Saunders1310@gmailo.com>
2021-11-25 19:25:19 +0200

    Added missing import.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { LandingPageComponent } from './components/landing-page/landing-page.com
 import { CareerPageComponent } from './components/career-page/career-page.component';
 import { ProjectsPageComponent } from './components/projects-page/projects-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatExpansionModule} from "@angular/material/expansion";
 @NgModule({
   declarations: [
     AppComponent,
